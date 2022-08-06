5786fd10c247cbd97040b9de43ae2ad0c2b08789
Athira Somaraj <athirasomaraj070gmail.com>
2022-08-06 15:10:06 +0530

    javascript added

---
+++ sample.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle=("white");
+ctx.fillRect(0,0,500,500);
+
+ctx.fillStyle="black";
+
+let brushColor = "black";
+let brushSize = 3;
+
+
+document.getElementById("color").addEventListener("change",function(){
+    brushColor = this.value;
+});
+
+document.getElementById("thickness").addEventListener("change",function(){
+    brushSize = this.value;
+});
+
+
+let painting = false;
+
+function paintingStart(e){
+    painting = true;
+    draw(e);
+    
+    
+}
+
+function paintingEnd(e){
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e){
+    if(painting==false) return;
+    let x=e.clientX;
+    let y=e.clientY - canvas.offsetTop;
+
+    ctx.lineWidth = brushSize;
+    ctx.lineCap = "round";
+    ctx.lineTo(x,y);
+    ctx.strokeStyle = brushColor;
+    ctx.stroke();
+
+}
+
+
+
+canvas.addEventListener("mousedown",paintingStart);
+canvas.addEventListener("mouseup",paintingEnd);
+canvas.addEventListener("mousemove",draw);
+
+
+
